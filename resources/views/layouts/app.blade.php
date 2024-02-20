c061538102cf781df14443566400e4f9a9e06169
Jlad <lanetejlad@gmail.com>
2024-02-21 01:44:58 +0800

    leaning about different types of routing

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    @include('layouts.nav')
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+</body>
+</html>
